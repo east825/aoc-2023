e635229c49e2ac57fb2cb3b02d27159b900211eb
Mikhail Golubev <qsolo825@gmail.com>
2023-12-03 17:41:37 +0200

    Day 2: extract parse_games()

--- day2.py
+++ day2.py
@@ -1,27 +1,41 @@
 from collections import Counter
-from typing import TextIO
+from dataclasses import dataclass
+from typing import TextIO, Literal
 
 from aoc_toolkit import open_puzzle_input
+
+Color = Literal["red", "blue", "green"]
+
+
+@dataclass
+class Game:
+    id: int
+    draw_sets: list[Counter[Color]]
+
+
+def parse_games(input_: TextIO) -> list[Game]:
+    games = []
+    for line in input_:
+        # E.g: Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        game_header, draw_sets_text = line.split(': ')
+        game_id = int(game_header.removeprefix('Game '))
+        draw_sets = []
+        for draw_set_text in draw_sets_text.split('; '):
+            num_color_pairs = [tuple(chunk.split()) for chunk in draw_set_text.split(', ')]
+            draw_sets.append(Counter({color: int(num) for num, color in num_color_pairs}))
+        games.append(Game(id=game_id, draw_sets=draw_sets))
+
+    return games
 
 
 def part1(input_: TextIO) -> int:
     bag = Counter({'red': 12, 'green': 13, 'blue': 14})
 
-    # E.g: Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-    possible_game_ids = []
-    for line in input_:
-        game_header, draw_sets_text = line.split(': ')
-        game_id = int(game_header.removeprefix('Game '))
-        draw_sets = []
-        for draw_set_text in draw_sets_text.split('; '):
-            color_draws = Counter()
-            for color_draw_text in draw_set_text.split(', '):
-                cube_num, cube_color = color_draw_text.split()
-                color_draws[cube_color] = int(cube_num)
-            draw_sets.append(color_draws)
-        if all(draw_set <= bag for draw_set in draw_sets):
-            possible_game_ids.append(game_id)
-    return sum(possible_game_ids)
+    def is_possible(game: Game):
+        return all(draw_set <= bag for draw_set in game.draw_sets)
+
+    games = parse_games(input_)
+    return sum(game.id for game in games if is_possible(game))
 
 
 if __name__ == '__main__':
