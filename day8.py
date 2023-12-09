a580dc6f92e21118a37152eced1b1a472710ee4e
Mikhail Golubev <qsolo825@gmail.com>
2023-12-09 23:06:17 +0200

    Day 8, part 2

--- day8.py
+++ day8.py
@@ -1,9 +1,12 @@
 import itertools
+import math
 
 from aoc_toolkit import open_puzzle_input, blank_separated_line_blocks
 
+Net = dict[str, tuple[str, str]]
 
-def _part_input(puzzle_input: list[list[str]]) -> tuple[str, dict[str, tuple[str, str]]]:
+
+def _parse_input(puzzle_input: list[list[str]]) -> tuple[str, Net]:
     instructions_block, net_block = puzzle_input
     instructions = instructions_block[0]
     net = {}
@@ -13,39 +16,33 @@ def _part_input(puzzle_input: list[list[str]]) -> tuple[str, dict[str, tuple[str
     return instructions, net  # type: ignore
 
 
-def part1(puzzle_input: list[list[str]]) -> int:
-    instructions, net = _part_input(puzzle_input)
-
-    cur_node = "AAA"
+def _walk(net: Net, instructions: str, start_node: str, until=lambda node: False):
+    cur_node = start_node
     steps = 0
     for move in itertools.cycle(instructions):
         left, right = net[cur_node]
         cur_node = right if move == "R" else left
         steps += 1
-        if cur_node == "ZZZ":
+        if until(cur_node):
             break
+    return steps, cur_node
+
+
+def part1(puzzle_input: list[list[str]]) -> int:
+    instructions, net = _parse_input(puzzle_input)
+    steps, _ = _walk(net, instructions, "AAA", lambda node: node == "ZZZ")
     return steps
 
 
 def part2(puzzle_input: list[list[str]]) -> int:
-    instructions, net = _part_input(puzzle_input)
-
-    cur_nodes = frozenset(node for node in net if node.endswith("A"))
-    steps = 0
-    # cache = set()
-    for move in itertools.cycle(instructions):
-        # state = frozenset(cur_nodes), move
-        # assert state not in cache
-        # cache.add(state)
-        cur_nodes = frozenset(net[node][1] if move == "R" else net[node][0] for node in cur_nodes)
-        steps += 1
-        if all(node.endswith("Z") for node in cur_nodes):
-            break
-    return steps
+    instructions, net = _parse_input(puzzle_input)
+    start_nodes = [node for node in net if node.endswith("A")]
+    cycle_lengths = [_walk(net, instructions, node, lambda node: node.endswith("Z"))[0] for node in start_nodes]
+    return math.lcm(*cycle_lengths)
 
 
 if __name__ == "__main__":
     with open_puzzle_input("day8") as f:
         puzzle_input = list(blank_separated_line_blocks(f))
-        # print(part1(puzzle_input))  # 13939
-        print(part2(puzzle_input))
+        print(part1(puzzle_input))  # 13939
+        print(part2(puzzle_input))  # 8906539031197
