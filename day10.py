1916295a50bbea8e3edcbcdf9718ff530a141d0b
Mikhail Golubev <qsolo825@gmail.com>
2023-12-12 11:03:22 +0200

    Day 10, part 2

--- day10.py
+++ day10.py
@@ -11,6 +11,16 @@ PIPE_CONNECTIONS = {
     ".": [],
 }
 
+PIPE_ASCII = {
+    "|": "│",
+    "-": "─",
+    "L": "└",
+    "J": "┘",
+    "7": "┐",
+    "F": "┌",
+    "S": "S",
+}
+
 
 def find_contour(field: list[str]) -> list[Pos]:
     start_pos = next(
@@ -44,13 +54,72 @@ def find_contour(field: list[str]) -> list[Pos]:
     return contour
 
 
+def draw_contour(field: list[str], contour: list[Pos], inner_tiles: set[Pos]) -> None:
+    contour_pipes = set(contour)
+    new_field = []
+    for row_idx, row in enumerate(field):
+        for col_idx, col in enumerate(row):
+            if Pos(row_idx, col_idx) in contour_pipes:
+                new_field.append(PIPE_ASCII[field[row_idx][col_idx]])
+            elif Pos(row_idx, col_idx) in inner_tiles:
+                new_field.append("I")
+            else:
+                new_field.append(" ")
+        new_field.append("\n")
+    with open("input/day10_prettified.txt", "w") as f:
+        f.write("".join(new_field))
+
+
+def find_inner_tiles(field: list[str], contour: list[Pos]) -> set[Pos]:
+    start_pos = contour[-1]
+    start_connections = {
+        Pos(contour[-2].col - start_pos.col, contour[-2].row - start_pos.row),
+        Pos(contour[0].col - start_pos.col, contour[0].row - start_pos.row),
+    }
+    s_replacement = next(
+        pipe
+        for pipe, conn in PIPE_CONNECTIONS.items()
+        if set(conn) == start_connections
+    )
+
+    inner_tiles = set()
+    contour_set = set(contour)
+    for row_idx, row in enumerate(field):
+        inside = False
+        prev_bend = None
+        for col_idx, col in enumerate(row):
+            pos = Pos(row_idx, col_idx)
+            if pos in contour_set:
+                pipe = field[row_idx][col_idx]
+                if pipe == "S":
+                    pipe = s_replacement
+                if pipe in "FL":
+                    prev_bend = pipe
+                elif pipe in "7J":
+                    assert prev_bend is not None
+                    if prev_bend + pipe in ["FJ", "L7"]:
+                        inside = not inside
+                elif pipe in "|":
+                    inside = not inside
+            elif inside:
+                inner_tiles.add(pos)
+    return inner_tiles
+
+
 def part1(puzzle_input: list[str]) -> int:
     contour = find_contour(puzzle_input)
     return len(contour) // 2
+
+
+def part2(puzzle_input: list[str]) -> int:
+    contour = find_contour(puzzle_input)
+    inner_tiles = find_inner_tiles(puzzle_input, contour)
+    # draw_contour(puzzle_input, contour, inner_tiles)
+    return len(inner_tiles)
 
 
 if __name__ == "__main__":
     with open_puzzle_input("day10") as f:
         puzzle_input = list(f.read().splitlines())
         print(part1(puzzle_input))  # 6697
-        # print(part2(puzzle_input))
+        print(part2(puzzle_input))
