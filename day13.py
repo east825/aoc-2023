2a42f523f5cc2447b466d1f899dbfc7518ea8709
Mikhail Golubev <qsolo825@gmail.com>
2024-01-06 11:35:58 +0200

    Day 13, part 1. Find maximal reflection.

--- day13.py
+++ day13.py
@@ -1,30 +1,28 @@
 from aoc_toolkit import open_puzzle_input, blank_separated_line_blocks
 
 
-def reflections2(block: list[str]) -> tuple[int, int]:
+def reflections(block: list[str]) -> tuple[int, int]:
     transposed = ["".join(row[col] for row in block) for col in range(len(block[0]))]
 
-    def is_reflection_below(row: int, block: list[str]) -> bool:
-        return all(r1 == r2 for r1, r2 in zip(block[row::-1], block[row + 1:]))
+    def is_reflection_above(refl_row: int, block: list[str]) -> bool:
+        return all(
+            r1 == r2 for r1, r2 in zip(block[refl_row - 1 :: -1], block[refl_row:])
+        )
 
-    def rows_above_reflection(b: list[str]) -> list[int]:
-        result = []
-        for row_above in range(len(b) - 1):
-            # Puzzle input guarantees there is only one reflection of each type per map
-            if is_reflection_below(row_above, b):
-                result.append(row_above + 1)
-        return result or [0]
+    def rows_above_reflection(block: list[str]) -> int:
+        for refl_size in range(len(block) // 2, 0, -1):
+            for row_under_refl in refl_size, len(block) - refl_size:
+                if is_reflection_above(row_under_refl, block):
+                    return row_under_refl
+        return 0
 
-    rows_above = rows_above_reflection(block)
-    cols_to_left = rows_above_reflection(transposed)
-    assert len(rows_above) == 1 and len(cols_to_left) == 1
-    return rows_above[0], cols_to_left[0]
+    return rows_above_reflection(block), rows_above_reflection(transposed)
 
 
 def part1(puzzle_input: list[list[str]]) -> int:
-    return sum(100 * rows + columns for rows, columns in map(reflections2, puzzle_input))
+    return sum(100 * rows + columns for rows, columns in map(reflections, puzzle_input))
 
 
 if __name__ == "__main__":
     with open_puzzle_input("day13") as f:
-        print(part1(list(blank_separated_line_blocks(f))))
+        print(part1(list(blank_separated_line_blocks(f))))  # 27742
