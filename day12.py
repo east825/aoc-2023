334366a245e7b3f4fbf855d1456d48729985c6e8
Mikhail Golubev <qsolo825@gmail.com>
2024-01-06 10:23:12 +0200

    Day 12. Don't return allocations as strings, only their number. Add part 2.

--- day12.py
+++ day12.py
@@ -13,28 +13,32 @@ def parse_input(puzzle_input: list[str]) -> list[tuple[str, tuple[int, ...]]]:
 
 
 @functools.cache
-def allocate(springs: str, groups: tuple[int, ...]) -> list[str]:
+def allocate(springs: str, groups: tuple[int, ...]) -> int:
     if not groups:
-        if "#" in springs:
-            return []
-        return [springs.replace("?", ".")]
-    first, *rest, = groups
-    result = []
+        return 0 if "#" in springs else 1
+    first, *rest = groups
+    result = 0
     for i in range(len(springs)):
-        if "#" in springs[:i] or not re.match(rf'[#?]{{{first}}}([.?]|$)', springs[i:]):
+        if "#" in springs[:i] or not re.match(rf"[#?]{{{first}}}([.?]|$)", springs[i:]):
             continue
-        partial = (springs[:i].replace("?", ".") +
-                   springs[i:i + first].replace("?", "#") +
-                   springs[i + first: i + first + 1].replace("?", "."))
-        result.extend([partial + suffix for suffix in allocate(springs[i + first + 1:], tuple(rest))])
+        result += allocate(springs[i + first + 1 :], tuple(rest))
     return result
 
 
 def part1(puzzle_input: list[str]):
     records = parse_input(puzzle_input)
-    return sum(len(list(allocate(*record))) for record in records)
+    return sum(allocate(*record) for record in records)
 
 
-if __name__ == '__main__':
+def part2(puzzle_input: list[str]):
+    records = parse_input(puzzle_input)
+    return sum(
+        allocate("?".join([springs] * 5), groups * 5) for springs, groups in records
+    )
+
+
+if __name__ == "__main__":
     with open_puzzle_input("day12") as f:
-        print(part1(f.read().splitlines()))
+        puzzle = f.read().splitlines()
+        print(part1(puzzle))  # 6981
+        print(part2(puzzle))  # 4546215031609
