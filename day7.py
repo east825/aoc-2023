dbd2ed7a9bc3837d90f8f527b2ec11d01be22df6
Mikhail Golubev <qsolo825@gmail.com>
2024-01-06 10:08:57 +0200

    Day 7. Use pattern matching

--- day7.py
+++ day7.py
@@ -24,25 +24,27 @@ class HandType(enum.IntEnum):
                 del counts["J"]
                 top_card = counts.most_common(1)
                 counts[top_card[0][0] if top_card else "A"] += jokers
-        card_counts = sorted(counts.values())
-        if card_counts == [5]:
-            return HandType.FIVE_OF_A_KIND
-        elif card_counts == [1, 4]:
-            return HandType.FOUR_OF_A_KIND
-        elif card_counts == [2, 3]:
-            return HandType.FULL_HOUSE
-        elif card_counts == [1, 1, 3]:
-            return HandType.THREE_OF_A_KIND
-        elif card_counts == [1, 2, 2]:
-            return HandType.TWO_PAIR
-        elif card_counts == [1, 1, 1, 2]:
-            return HandType.ONE_PAIR
-        elif card_counts == [1, 1, 1, 1, 1]:
-            return HandType.HIGH_CARD
+        match sorted(counts.values()):
+            case [5]:
+                return HandType.FIVE_OF_A_KIND
+            case [1, 4]:
+                return HandType.FOUR_OF_A_KIND
+            case [2, 3]:
+                return HandType.FULL_HOUSE
+            case [1, 1, 3]:
+                return HandType.THREE_OF_A_KIND
+            case [1, 2, 2]:
+                return HandType.TWO_PAIR
+            case [1, 1, 1, 2]:
+                return HandType.ONE_PAIR
+            case [1, 1, 1, 1, 1]:
+                return HandType.HIGH_CARD
         raise ValueError
 
 
-def hand_strength(hand: str, with_jokers=False) -> tuple[HandType, int, int, int, int, int]:
+def hand_strength(
+    hand: str, with_jokers=False
+) -> tuple[HandType, int, int, int, int, int]:
     cards_by_strength = "J23456789TJQKA" if with_jokers else "23456789TJQKA"
     return HandType.of_hand(hand, with_jokers), *(cards_by_strength.index(c) for c in hand)  # type: ignore
 
@@ -57,13 +59,19 @@ def _parse_input(puzzle_input: list[str]) -> list[tuple[int, int]]:
 
 def part1(puzzle_input: list[str]) -> int:
     hand_to_bid = _parse_input(puzzle_input)
-    ranks = sorted((hand for hand, _ in hand_to_bid), key=lambda h: hand_strength(h, with_jokers=False))
+    ranks = sorted(
+        (hand for hand, _ in hand_to_bid),
+        key=lambda h: hand_strength(h, with_jokers=False),
+    )
     return sum((ranks.index(hand) + 1) * bid for hand, bid in hand_to_bid)
 
 
 def part2(puzzle_input: list[str]) -> int:
     hand_to_bid = _parse_input(puzzle_input)
-    ranks = sorted((hand for hand, _ in hand_to_bid), key=lambda h: hand_strength(h, with_jokers=True))
+    ranks = sorted(
+        (hand for hand, _ in hand_to_bid),
+        key=lambda h: hand_strength(h, with_jokers=True),
+    )
     return sum((ranks.index(hand) + 1) * bid for hand, bid in hand_to_bid)
 
 
